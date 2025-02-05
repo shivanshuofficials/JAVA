7bf81ba4b156ee846be5ba8b091e29b3d6dc64c5
shivanshuofficials <shivanshuchauhanofficials@gmail.com>
2025-02-05 23:08:26 +0530

    sorting

---
+++ basic_sorting/counting_sort.java
@@ -0,0 +1,19 @@
+package basic_sorting;
+import java.util.*;
+public class counting_sort {
+    public static int countingsort(int arr[]) {
+        //code
+        return 0;
+    }
+
+    public static void print(int arr[]) {
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {4, 2, 2, 8, 3,7};
+        countingsort(arr);
+    }
+}
