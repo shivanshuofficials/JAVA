79b73dff62edea00873c3b7618cd32b52535cae4
shivanshuofficials <shivanshuchauhanofficials@gmail.com>
2025-02-18 23:33:45 +0530

    Bit manipulation

--- Bit_manipulation/binary_and.java
+++ Bit_manipulation/binary_and.java
@@ -7,7 +7,7 @@ public class binary_and {
         // 0,1 -> 0
         // 1,0 -> 0
         // 1,1 -> 1
-        
-        System.out.println(5 & 6);
+
+        System.out.println((5 & 6));
     }
 }
