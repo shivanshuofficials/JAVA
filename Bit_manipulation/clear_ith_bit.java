fb92406ab7a79b093beaceb09cc6b0e36bb873a7
shivanshuofficials <shivanshuchauhanofficials@gmail.com>
2025-03-02 00:35:23 +0530

    bit manipulation

--- Bit_manipulation/clear_ith_bit.java
+++ Bit_manipulation/clear_ith_bit.java
@@ -2,10 +2,15 @@ package Bit_manipulation;
 
 public class clear_ith_bit {
     public static int clear(int n ,int i) {
-        int bitmask = ~(1<<i);
+        int bitmask = (~0)<<i;
         return n & bitmask;
+        //~0 = 11111111
     }
     public static void main(String[] args) {
-        System.out.println( clear(10, 1) );
+        System.out.println( clear(15, 2) );
+        // 15 = 1111
+        // 2nd bit from right is 0
+        // 12 = 1100
     }
 }
+//15 = 1111 = 1100
